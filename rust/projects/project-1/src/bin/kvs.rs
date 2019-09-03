8002722a9a5e30fe3304b2f75b357b5af428a8b6
pentium3 <celerond@msn.com>
2019-09-03 00:43:26 -0500

    project1

--- rust/projects/project-1/src/bin/kvs.rs
+++ rust/projects/project-1/src/bin/kvs.rs
@@ -1,2 +1,29 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+use std::process::exit;
+
 fn main() {
+    let yaml = load_yaml!("cli.yml");
+    let m = App::from_yaml(yaml).get_matches();
+
+    let _v = match m.occurrences_of("V"){
+        _ => println!("0.1.0")
+    };
+
+    match m.subcommand() {
+        ("set", Some(_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("get", Some(_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("rm", Some(_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        _ => unreachable!(),
+    }
 }
