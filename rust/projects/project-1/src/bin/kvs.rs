93cc8b2398b6e05b57374a86fa8df32c2a4ee97f
Neil_Cai <779121147@qq.com>
2020-02-19 15:42:03 +0800

    neil_p1_t5

--- rust/projects/project-1/src/bin/kvs.rs
+++ rust/projects/project-1/src/bin/kvs.rs
@@ -1,2 +1,29 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+use std::process::exit;
+
+
 fn main() {
+    // The YAML file is found relative to the current file, similar to how modules are found
+    let yaml = load_yaml!("../kvs.yml");
+    let m = App::from_yaml(yaml).get_matches();
+
+    // get_matches 正则花匹配
+    match m.subcommand() {
+        ("set", Some(_)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("get", Some(_)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("rm", Some(_)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        _ => unreachable!(),
+    }
+
 }
--- rust/projects/project-1/src/lib.rs
+++ rust/projects/project-1/src/lib.rs
@@ -1,5 +1,9 @@
-//! A simple key/value store.
 
-pub use kv::KvStore;
+//#![deny(missing_docs)]
 
-mod kv;
+//!kvs是一个内存存储的数据库，支持set，get，remove操作。不具有持久性。
+
+mod kv_engine;
+
+pub use kv_engine::KvStore;
+
