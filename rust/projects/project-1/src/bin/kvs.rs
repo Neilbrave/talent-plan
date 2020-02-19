08f66c17cbcf83de28b83a141b5a374e42042cfb
Neil_Cai <779121147@qq.com>
2020-02-19 16:30:02 +0800

    neil_p1

--- rust/projects/project-1/src/bin/kvs.rs
+++ rust/projects/project-1/src/bin/kvs.rs
@@ -3,8 +3,6 @@ extern crate clap;
 use clap::App;
 use std::process::exit;
 
-<<<<<<< HEAD
-
 fn main() {
     // The YAML file is found relative to the current file, similar to how modules are found
     let yaml = load_yaml!("../kvs.yml");
@@ -21,33 +19,9 @@ fn main() {
             exit(1);
         }
         ("rm", Some(_)) => {
-=======
-fn main() {
-    let yaml = load_yaml!("cli.yml");
-    let m = App::from_yaml(yaml).get_matches();
-
-    let _v = match m.occurrences_of("V"){
-        _ => println!("0.1.0")
-    };
-
-    match m.subcommand() {
-        ("set", Some(_matches)) => {
-            eprintln!("unimplemented");
-            exit(1);
-        }
-        ("get", Some(_matches)) => {
-            eprintln!("unimplemented");
-            exit(1);
-        }
-        ("rm", Some(_matches)) => {
->>>>>>> 8002722a9a5e30fe3304b2f75b357b5af428a8b6
-            eprintln!("unimplemented");
+           eprintln!("unimplemented");
             exit(1);
         }
         _ => unreachable!(),
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 8002722a9a5e30fe3304b2f75b357b5af428a8b6
 }
